dcee8485c38443b917fff493afe025d4b060cc7b
NoharOSP <thegeminireport@gmail.com>
2025-11-25 21:59:25 +0100

    Remove redundant variables

--- source/main.h
+++ source/main.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "resource.h"
 #include <shellapi.h>
 #include <commdlg.h>
 #include <iostream>
@@ -11,22 +10,14 @@
 #include <cstdlib>
 #include <cstdio>
 #include <direct.h>
+#include "resource.h"
 #include "toolGenerator.h"
 #include "Window.h"
 #include "framework.h"
 
 #define MAX_LOADSTRING 100
-#define BUF_SIZE 300
-#define WM_UPDATEPROGRESS WM_USER + 1
 
 #pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
-
-typedef std::basic_string<TCHAR> ustring;
-
-enum {
-	IDC_TAB = 200,
-};
-
 
 // Global variables
 HINSTANCE hInst;                                // current instance
@@ -36,21 +27,6 @@ WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
 Window* pWin;
 int winX = 800;
 int winY = 600;
-int id;
-int workingDisc;
-bool changed;
-bool space = false;
-bool guidrawn = false;
-bool globdrawn = false;
-bool safeDrive = false;
-std::string newPath1 = "";
-std::string newPath2 = "";
-HWND window;
-HWND hwndPB;
-HWND tc;
-RECT rc;
-HDC hdc_new;
-HBRUSH hBrush;
 
 // Global methods
 ATOM                MyRegisterClass(HINSTANCE hInstance);
